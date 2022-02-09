bd41fe9c95145c46735f53033c0a8508f952ed16
mghebreslassie <messeleghebreslassie@gmail.com>
2022-02-08 20:32:35 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,11 @@ import {
 } from "./styles/characterinfostyle";
 import CharacterContainer from "./components/characterContainer";
 
+import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 const App: React.FC = () => {
   const [currentMarker, setCurrentMarker] = useState("");
   const [charObjs] = useState(characterData);
